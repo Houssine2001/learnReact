1bb7c627b24ca6e0e17f820e56f2e5cbdc10ecd4
houssine125 <houssinebenarous48@gmail.com>
2024-07-12 17:40:49 +0200

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+    // JSX: Javascript XML
+    const name = 'Houssine'
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello {name}</h1>; 
+}
+
+export default Message;
